d1a191b6a8eb0402fc86e80956a93faff19f8e33
Stefan <Stefan@DESKTOP-AKUT48I>
2023-07-06 15:20:09 +0300

    Add project files.

---
+++ apl1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using Microsoft.Win32;
+using System;
+
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace apl1
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey regkey = Registry.CurrentUser.OpenSubKey(@"Control Panel\International", true);
+            regkey.SetValue("sShortDate", "MM/yyyy/d");
+            
+            //Process.Start("explorer.exe", "/n /r ");
+           
+            KillExplorerProcess();
+        }
+        private void KillExplorerProcess()
+        {
+            Process[] explorerProcesses = Process.GetProcessesByName("explorer");
+            foreach (Process process in explorerProcesses)
+            {
+                process.Kill();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey regkey = Registry.CurrentUser.OpenSubKey(@"Control Panel\International", true);
+            regkey.SetValue("sShortDate", "d/MM/yyyy");
+            //  regkey.SetValue("sLongDate", "dddd,MMMMM d,yyyy ");
+            KillExplorerProcess();
+
+
+        }
+    }
+}
